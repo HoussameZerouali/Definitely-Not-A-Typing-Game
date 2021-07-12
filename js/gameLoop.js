ec0c3ef2b17ff63a9198cdd810160678de87b1f9
Houssame Zerouali <azenax328@gmail.com>
2021-07-12 09:54:11 +0200

    added the base game logic

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -20,22 +20,30 @@ class scissors {
     }
 }
 
-const cardsClicker = document.querySelectorAll(".card-div")
+const cardsClicker = document.querySelectorAll(".card-div");
+const p1Card1 = document.querySelector(".p1-card1");
+const p1Card2 = document.querySelector(".p1-card2");
+const p1Card3 = document.querySelector(".p1-card3");
 
 
 //Initializing the basic parameters
-let deck = []
+let deck = [];
 let typeOfCards = [rock,paper,scissors];
-let p1Deck = []
-let p2Deck = []
+let p1Deck = [];
+let p2Deck = [];
+let p1ChosenCard;
+let p2ChosenCard;
 let p1Score = 0;
 let p2Score = 0;
 
 
 //Main Game Loop 
 function gameLoop(){
-    gameStart()
-    drawHand(p1Deck,p2Deck)
+    gameStart();
+    drawHand(p1Deck,p2Deck);
+    choseCard();
+    
+    
 
 }
 //Creating a random deck of the different type of cards
@@ -75,22 +83,49 @@ function drawHand(p1Deck,p2Deck){
         
     }
 
+    p1Card1.classList.add(p1Deck[0].name);
+    p1Card2.classList.add(p1Deck[1].name);
+    p1Card3.classList.add(p1Deck[2].name);
+
     console.log("First player hand is :",p1Deck)
     console.log("Second Player Hand is :",p2Deck)
     console.log("Cards left in the deck :",deck)
 }
 
+function choseCard() {
+    let randomIndex = Math.floor(Math.random() * 3)
+    p2ChosenCard = p2Deck[randomIndex];
+    
+
+    p1Card1.addEventListener("click",() => {
+        p1ChosenCard = p1Deck[0]
+        duel(p1ChosenCard,p2ChosenCard)
+        
+    })
+    p1Card2.addEventListener("click",() => {
+        p1ChosenCard = p1Deck[1]
+        duel(p1ChosenCard,p2ChosenCard)
+    })
+    p1Card3.addEventListener("click",() => {
+        p1ChosenCard = p1Deck[2]
+        duel(p1ChosenCard,p2ChosenCard)
+    })
+
+    
+}
+
 // this will compare the 2 cards picked by the player to decide who wins the rock-paper-scissors
 function duel(p1Card,p2Card){
-    if (p1Card.name == p2Card.weakerThan){
+    if (p1Card.name == p2Card.weak){
         p1Score += 1;
         console.log("Player 1 won the duel")
 
-    }else if(p2Card.name == p1Card.weakerThan){
+    }else if(p2Card.name == p1Card.weak){
         p2Score += 1;
         console.log("Player 2 won the duel")
     }else{
-        
+        console.log(p1Card)
+        console.log(p2Card)
         console.log("Draw")
     }
 }
