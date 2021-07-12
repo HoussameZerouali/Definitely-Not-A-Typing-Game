1b743ef0957ba8f8412895f15f490fc224c20d75
Houssame Zerouali <azenax328@gmail.com>
2021-07-12 19:03:12 +0200

    finished the game logic now will work on the CSS and styling

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -24,7 +24,10 @@ const cardsClicker = document.querySelectorAll(".card-div");
 const p1Card1 = document.querySelector(".p1-card1");
 const p1Card2 = document.querySelector(".p1-card2");
 const p1Card3 = document.querySelector(".p1-card3");
-
+const confirmBtn = document.querySelector(".glow-on-hover")
+const scoreField = document.querySelector(".score-field")
+const p1CardPlayed = document.querySelector(".p1ChosenCard")
+const p2CardPlayed = document.querySelector(".p2ChosenCard")
 
 //Initializing the basic parameters
 let deck = [];
@@ -49,7 +52,7 @@ function gameLoop(){
 //Creating a random deck of the different type of cards
 function gameStart(){
    
-    for (i=0;i<10;i++){
+    for (i=0;i<48;i++){
         
         let card = new typeOfCards[Math.floor(Math.random() * 3)]
         deck.push(card)
@@ -92,6 +95,8 @@ function drawHand(p1Deck,p2Deck){
     console.log("Cards left in the deck :",deck)
 }
 
+
+// This function lets us pick a card for the AI and then let the player chose his own card and then make them duel
 function choseCard() {
     let randomIndex = Math.floor(Math.random() * 3)
     p2ChosenCard = p2Deck[randomIndex];
@@ -99,16 +104,22 @@ function choseCard() {
 
     p1Card1.addEventListener("click",() => {
         p1ChosenCard = p1Deck[0]
-        duel(p1ChosenCard,p2ChosenCard)
         
+        p1Card1.classList.add("isSelected")
+        p1Card2.classList.remove("isSelected")
+        p1Card3.classList.remove("isSelected")
     })
     p1Card2.addEventListener("click",() => {
         p1ChosenCard = p1Deck[1]
-        duel(p1ChosenCard,p2ChosenCard)
+        p1Card2.classList.add("isSelected")
+        p1Card1.classList.remove("isSelected")
+        p1Card3.classList.remove("isSelected")
     })
     p1Card3.addEventListener("click",() => {
         p1ChosenCard = p1Deck[2]
-        duel(p1ChosenCard,p2ChosenCard)
+        p1Card3.classList.add("isSelected")
+        p1Card1.classList.remove("isSelected")
+        p1Card2.classList.remove("isSelected")
     })
 
     
@@ -118,20 +129,47 @@ function choseCard() {
 function duel(p1Card,p2Card){
     if (p1Card.name == p2Card.weak){
         p1Score += 1;
-        console.log("Player 1 won the duel")
-
+        
+        scoreField.textContent = `SCORE : ${p1Score}:${p2Score}`;
     }else if(p2Card.name == p1Card.weak){
         p2Score += 1;
-        console.log("Player 2 won the duel")
+        
+        scoreField.textContent = `SCORE : ${p1Score}:${p2Score}`;
     }else{
-        console.log(p1Card)
-        console.log(p2Card)
+        
         console.log("Draw")
     }
 }
 
 
+
+
+confirmBtn.addEventListener("click",() => {
+    p1CardPlayed.classList.add(p1ChosenCard.name);
+    p2CardPlayed.classList.add(p2ChosenCard.name)
+    duel(p1ChosenCard,p2ChosenCard);
+    p1Deck = [];
+    p2Deck = [];
+    
+    cardsClicker.forEach(element => {
+        element.classList.remove("rock","paper","scissors")
+    });
+    
+    if (deck.length > 0){
+        drawHand(p1Deck,p2Deck);
+        p1Card1.classList.remove("isSelected")
+        p1Card2.classList.remove("isSelected")
+        p1Card3.classList.remove("isSelected")
+    }else{
+        alert("Deck is empty")
+    }
+    
+    setTimeout(() => {
+        p1CardPlayed.classList.remove("rock","paper","scissors")
+        p2CardPlayed.classList.remove("rock","paper","scissors")
+    }, 2000);
+})
+
+
+
 gameLoop();
-
-
-
